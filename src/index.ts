1833ed5b80714038e4bd1e85245ad6ee949f3f36
Andre Ekbom <andre.ekbom@dreamlake.com>
2023-02-04 11:30:57 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './loki-logger.module';
 export * from './loki-logger.service';
 export * from './loki-logger.constants';
 export * from './loki-logger.interface';
+export * from './loki-logger.enum';
--- src/loki-logger.enum.ts
+++ src/loki-logger.enum.ts
@@ -1,7 +1,9 @@
 export enum LogLevel {
-  log = 0,
-  verbose = 1,
-  debug = 2,
-  warn = 3,
-  error = 4,
+  error = 0,
+  warn = 1,
+  info = 2,
+  http = 3,
+  verbose = 4,
+  debug = 5,
+  silly = 6,
 }
